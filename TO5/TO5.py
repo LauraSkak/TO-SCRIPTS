1dd60ac9e6624ff841b78ef8e3712d53d2ecca72
Laura Arberg Nieboer <laura_nieboer@hotmail.com>
2023-09-29 08:27:04 +0200

    sup

--- TO5/TO5.py
+++ TO5/TO5.py
@@ -5,10 +5,8 @@ reversed=False
 if reversed == False:
     midtpoint = len(n)//2
     for i in range(midtpoint):
-        print(i)
         n_ordered.insert(i, n[i])
         n_ordered.insert(i, n[(len(n)-1-i)])
-        print(n_ordered)
     if len(n)%2 != 0:
         n_ordered.insert(midtpoint, n[midtpoint]) 
 
