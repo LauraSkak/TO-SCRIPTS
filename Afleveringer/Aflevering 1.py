f0255178d145016c20bce5301a966b16793f51f5
CecilieSvendsen <ceciliesvendsen00@gmail.com>
2023-09-29 09:56:10 +0200

    merge

--- Afleveringer/Aflevering 1.py
+++ Afleveringer/Aflevering 1.py
@@ -19,3 +19,21 @@ def merge(x:list[int],y:list[int]) ->list[int]:
 x=[1,2,4,6]
 y=[1,3,4,5]
 print(merge(x,y))
+
+#Cecilies kode
+
+def merge(x: list[int], y: list[int]) -> list[int]:
+    i, j = 0, 0
+    z = []  
+    while i < len(x) and j < len(y):
+        if x[i]<y[j]:
+            z.append(x[i])
+            i+=1
+        else:
+            z.append(y[j])
+            j+=1
+    if i==len(x):
+        z=z+y[j:]
+    else:
+        z=z+x[i:]
+    return z
