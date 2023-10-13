7c3ae6115b01516628733d8327db0fb796ee7464
Laura Arberg Nieboer <laura_nieboer@hotmail.com>
2023-10-13 12:12:58 +0200

    +exercise 8

--- TO7/exercises.py
+++ TO7/exercises.py
@@ -4,9 +4,10 @@ class Vehicle:
         self.name = name
         self.seats = seats
         self.trunk_space = trunk_space
+        #Exercise 5
         self.fuel = 'gasoline'
 
-    #exercise 2
+    #Exercise 2
 
     def __str__(self):
         return self.name
@@ -14,7 +15,7 @@ class Vehicle:
     #Exercise 3
 
     def at_capacity(self, passengers, suitcases):
-        if passengers >= self.seats:
+        if passengers > self.seats:
             return True
         elif suitcases >= self.trunk_space + (self.seats-passengers):
             return True
@@ -28,14 +29,20 @@ class Vehicle:
 #car = Vehicle("Jeep", 4, 2)
 #print(car)
 
-#car = Vehicle("Volvo", 4, 3)
-#print(f'It is {car.at_capacity(3, 3)} that the {car.name} is at/over capacity')
+car = Vehicle("Volvo", 4, 3)
+print(f'It is {car.at_capacity(4, 2)} that the {car.name} is at/over capacity')
 
+#Exercise 4
 class Bus(Vehicle):
+    #exercise 5 kan også skrives udenfor init:
+    #fuel='diesel'
+
+    #Behøver ikke at definere dette, da det bare kan komme fra Vehicle)
     def __init__(self, name, seats, trunk_space):
         self.name = name
         self.seats = seats
         self.trunk_space = trunk_space
+        #Exercise 5
         self.fuel = 'diesel'
     
     def at_capacity(self, passengers, suitcases):
@@ -81,4 +88,91 @@ auto_trans.load(Vehicle('Mustang', 4, 1))
 auto_trans.load(Vehicle('Charger', 4, 1))
 auto_trans.load(Vehicle('Corvette', 4, 1))
 auto_trans.load(Vehicle('Challenger', 4, 1))
-print(auto_trans)
+#print(auto_trans)
+
+#Exercise 8
+class DNA:
+    def __init__(self, string):
+        self.string = string
+    
+    def count_nucleotides(self):
+        nucleotides_dict={'A':0, 'T':0, 'C':0, 'G':0}
+        for n in self.string:
+            nucleotides_dict[n]+=1
+        return nucleotides_dict
+    
+    def GC_content(self):
+        count_dict=self.count_nucleotides()
+        GC_count=count_dict['C']+count_dict['G']
+        count_sum=count_dict['C']+count_dict['G']+count_dict['A']+count_dict['T']
+        return GC_count/count_sum
+    
+    def codons(self):
+        codon_list=[]
+        for i in range(0,len(self.string)-2,3):
+            codon_list.append(self.string[i:i+3])
+        return codon_list
+
+    def protein_seq(self):
+        CODON_MAP = {'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L',
+             'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S', 
+             'TAT': 'Y', 'TAC': 'Y', 'TAA': '*', 'TAG': '*', 
+             'TGT': 'C', 'TGC': 'C', 'TGA': '*', 'TGG': 'W', 
+             'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
+             'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P', 
+             'CAT': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q', 
+             'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 
+             'ATT': 'I', 'ATC': 'I', 'ATA': 'I', 'ATG': 'M', 
+             'ACT': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T', 
+             'AAT': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K', 
+             'AGT': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R', 
+             'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V', 
+             'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A', 
+             'GAT': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E', 
+             'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'}
+        
+        protein=''
+        codon_list=self.codons()
+        for codon in codon_list:
+            protein+=CODON_MAP[codon]
+        return protein
+    
+    def reverse_complement(self):
+        reverse_string=''
+        complement_dict={'A':'T','T':'A', 'C':'G', 'G':'C'}
+        for i in range(len(self.string)-1,-1,-1):
+            reverse_string+=complement_dict[self.string[i]]
+        return reverse_string
+
+#class RNA(DNA):
+    # #def __init__(self, string):
+    # #    if 'T' in string:
+    #         self.DNA=string
+    #     else:
+    #         DNA_string=''
+    #         for base in string:
+                 
+    #         self.DNA=
+    #     return None
+    
+    # def reverse_complemenent(self):
+    #     reverse_string=''
+    #     complement_dict={'A':'U','U':'A', 'C':'G', 'G':'C'}
+    #     for i in range(len(self.string)-1,-1,-1):
+    #         reverse_string+=complement_dict[self.string[i]]
+    #     return reverse_string
+    
+    # #def codons(self):
+
+
+
+string=DNA('ATGGGCTTGACT')
+
+#print(string.count_nucleotides())
+#print(string.GC_content())
+#print(string.codons())
+#print(string.protein_seq())
+#print(string.reverse_complement())
+
+
+
